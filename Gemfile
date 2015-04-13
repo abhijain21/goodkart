35cd49198d756002c5ac44e8a49033697240419f
abhishek jain <ajain@looksmart.net>
2015-04-13 11:00:26 +0530

    basic sphinx integration

--- Gemfile
+++ Gemfile
@@ -5,8 +5,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
-gem 'mysql2'
+gem 'mysql2', :platform => :ruby
 gem 'puma'
+gem 'thinking-sphinx', '3.1.0'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :format, :pretty
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -63,7 +63,6 @@ namespace :deploy do
   end
 
   after :publishing, :restart
-  after :publishing, puma:restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
